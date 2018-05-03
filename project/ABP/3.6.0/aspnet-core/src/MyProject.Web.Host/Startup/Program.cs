c18090fabf091aaca38716962e26646121ee2aaa
miaorong <1510894530@qq.com>
2018-05-03 18:08:42 +0800

    add

--- project/ABP/3.6.0/aspnet-core/src/MyProject.Web.Host/Startup/Program.cs
+++ project/ABP/3.6.0/aspnet-core/src/MyProject.Web.Host/Startup/Program.cs
@@ -7,9 +7,11 @@ namespace MyProject.Web.Host.Startup
     {
         public static void Main(string[] args)
         {
+            //BuildWebHost(args).Run();
             InitWebHost(args).Run();
         }
 
+        //  public static IWebHost BuildWebHost(string[] args)
         public static IWebHost InitWebHost(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
